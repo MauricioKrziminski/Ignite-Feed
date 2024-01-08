15a6f39b0a88dba604b41f6f0f273faf54f63e7c
MauricioKrziminski <maumausk2015@gmail.com>
2024-01-08 02:31:47 -0300

    fix: adjustments to conflicts

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,13 @@
-import { PiPencilSimpleLine } from "react-icons/pi";
-import styles from './Sidebar.module.css'
-import { Avatar } from "./Avatar";
+import { PencilLine } from "phosphor-react"
+import styles from "./Sidebar.module.css"
+import { Avatar } from "./Avatar"
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img 
-        className={styles.cover}src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
       <div className={styles.profile}>
@@ -17,7 +18,7 @@ export function Sidebar() {
 
       <footer>
         <a href="#">
-          <PiPencilSimpleLine size={20}/>
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
